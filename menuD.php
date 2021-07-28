df4f8a97c0da8ad4a98ef534fd54663458e43b2a
Dylancouzon <dylancouzon@gmail.com>
2021-07-28 14:29:30 -0700

    Fix right menu

--- menuD.php
+++ menuD.php
@@ -6,87 +6,34 @@
 		</tr>
 		<tr>
 			<td class='statut_middle'>
-            <div id='serveur'>
+            <div id='serveur' >
             	<?php
-				@$fp = fsockopen($URLLogin, $PortLogin, $errno, $errstr, 1);
-				if(!$fp)
-				{
-				 $statut="off";
-				} else {
 				 $statut="on";
-				}
 				?>
                 <span class='nom_royaume'>Set realmlist login.feriale.net </span><br />
                	<img class='statut_serveur' src='images/theme/defaut/<?php echo $statut; ?>.PNG' />
 				</div>
 				<br /><br />						
-	
-				<?php
-                $nta2 =0 ;
-				$nth2 =0 ;
-				$ntj =0 ;
-				$IDRoyaumeSite= 1;
-				foreach($sql_royaume as $key=>$value){
-					if($value[4]){
-				
-	$connect = connection($DBRealmd);
-		$statutR = mysql_query("SELECT * FROM realmlist WHERE id = ".$key." AND color = 0 ORDER by name ASC");
-		mysql_close($connect);
-		$statutT = mysql_fetch_array($statutR);
-				?>
-				
-				
+		
+
 				<div id='serveur'>
-                <img class='type_royaume' src='images/theme/defaut/logo_type_<?php echo $value[5]; ?>.PNG' />
-                <span class='nom_royaume'><?php echo utf8_decode($value[6]); ?></span>
-                
-                <?php
-				@$fp = fsockopen($statutT['address'], $statutT['port'], $errno, $errstr, 1);
-				if(!$fp)
-				{
-				 $statut="off";
-				} else {
-				 $statut="on";
-				}
-				?>
-                <div style='margin-top:0px'>
-               	<img class='statut_serveur' src='images/theme/defaut/<?php echo $statut; ?>.PNG' />
+                <img class='type_royaume' src='images/theme/defaut/logo_type_0.PNG' />
+                <span class='nom_royaume'>First Realm</span>
+
+                <div style='margin-top:-2px'>
+               	<img class='statut_serveur' src='images/theme/defaut/on.PNG' />
                 <table width='160px' style='position:relative; left:70px; top:-18px;'>
                 <tr><td width='25%'>
                 <img class='logo_faction' src='images/theme/defaut/logo_alliance.PNG ' />
-                 
-         <?php
-		 if($statut=="on")
-		 {
-		$connect = connection(false,$sql_royaume,$key);   
-        $req3 = mysql_query("SELECT COUNT(*) as nombrea2 FROM characters WHERE race = 1 AND online = 1 OR race = 3 AND online = 1  OR race = 4 AND online = 1  OR race = 7 AND online = 1  OR race = 11 AND online = 1 ");
-        $row3 = mysql_fetch_array($req3);
-        $na2 = $row3['nombrea2']; 
-		}
-		else{
-		$na2=0;
-		}
-		?>
-                 <span style='font-size: 9px; position: relative; top:-4px; left:-3px'><?php echo $na2; ?></span>
+                 <span style='font-size: 9px; position: relative; top:-4px; left:-3px'>100</span>
                  </td><td width='30%'>
                  <img class='logo_faction' src='images/theme/defaut/logo_horde.PNG ' />
-                 
-         <?php
-		 if($statut=="on")
-		 {
-		$connect = connection(false,$sql_royaume,$key);
-        $req3 = mysql_query("SELECT COUNT(*) as nombreh2 FROM characters WHERE race = 2 AND online = 1 OR race = 5 AND online = 1  OR race = 6 AND online = 1  OR race = 8 AND online = 1  OR race = 10 AND online = 1 ");
-        $row3 = mysql_fetch_array($req3);
-        $nh2 = $row3['nombreh2'];
-		}
-		$nt=0;
-		$nt = $na2+$nh2;
-		?>
-                 <span style='font-size: 9px; position: relative; top:-4px; left:-3px'><?php echo $nh2; ?></span>
+
+                 <span style='font-size: 9px; position: relative; top:-4px; left:-3px'>50</span>
                  </td><td width='40%'>
                  <span class='test' style='position:relative;'>
                  <img class='logo_faction' src='images/theme/defaut/logo_total.PNG' />
-                 <span style='font-size: 11px; position: relative; top:-4px; left:-3px'><strong><?php echo $nt; ?></strong></span>
+                 <span style='font-size: 11px; position: relative; top:-4px; left:-3px'><strong>150</strong></span>
                  </span>
                  </td>
                  </tr>
@@ -94,20 +41,89 @@
 				</div>
                 </div>
 				<br />
-				<?php 
-				$nta2=$nta2+$na2;
-				$nth2=$nth2+$nh2;
-				$IDRoyaumeSite++;
-				}
-				} ?>
+
+				<div id='serveur'>
+                <img class='type_royaume' src='images/theme/defaut/logo_type_1.PNG' />
+                <span class='nom_royaume'>Second Realm</span>
+
+                <div style='margin-top:-2px'>
+               	<img class='statut_serveur' src='images/theme/defaut/on.PNG' />
+                <table width='160px' style='position:relative; left:70px; top:-18px;'>
+                <tr><td width='25%'>
+                <img class='logo_faction' src='images/theme/defaut/logo_alliance.PNG ' />
+                 <span style='font-size: 9px; position: relative; top:-4px; left:-3px'>100</span>
+                 </td><td width='30%'>
+                 <img class='logo_faction' src='images/theme/defaut/logo_horde.PNG ' />
+
+                 <span style='font-size: 9px; position: relative; top:-4px; left:-3px'>50</span>
+                 </td><td width='40%'>
+                 <span class='test' style='position:relative;'>
+                 <img class='logo_faction' src='images/theme/defaut/logo_total.PNG' />
+                 <span style='font-size: 11px; position: relative; top:-4px; left:-3px'><strong>150</strong></span>
+                 </span>
+                 </td>
+                 </tr>
+                 </table>
+				</div>
+                </div>
+				<br />
+
+				<div id='serveur'>
+                <img class='type_royaume' src='images/theme/defaut/logo_type_2.PNG' />
+                <span class='nom_royaume'>Third Realm</span>
+
+                <div style='margin-top:-2px'>
+               	<img class='statut_serveur' src='images/theme/defaut/off.PNG' />
+                <table width='160px' style='position:relative; left:70px; top:-18px;'>
+                <tr><td width='25%'>
+                <img class='logo_faction' src='images/theme/defaut/logo_alliance.PNG ' />
+                 <span style='font-size: 9px; position: relative; top:-4px; left:-3px'>0</span>
+                 </td><td width='30%'>
+                 <img class='logo_faction' src='images/theme/defaut/logo_horde.PNG ' />
+
+                 <span style='font-size: 9px; position: relative; top:-4px; left:-3px'>0</span>
+                 </td><td width='40%'>
+                 <span class='test' style='position:relative;'>
+                 <img class='logo_faction' src='images/theme/defaut/logo_total.PNG' />
+                 <span style='font-size: 11px; position: relative; top:-4px; left:-3px'><strong>0</strong></span>
+                 </span>
+                 </td>
+                 </tr>
+                 </table>
+				</div>
+                </div>
+				<br />
+
+				<div id='serveur'>
+                <img class='type_royaume' src='images/theme/defaut/logo_type_3.PNG' />
+                <span class='nom_royaume'>Fourth Realm</span>
+
+                <div style='margin-top:-2px'>
+               	<img class='statut_serveur' src='images/theme/defaut/on.PNG' />
+                <table width='160px' style='position:relative; left:70px; top:-18px;'>
+                <tr><td width='25%'>
+                <img class='logo_faction' src='images/theme/defaut/logo_alliance.PNG ' />
+                 <span style='font-size: 9px; position: relative; top:-4px; left:-3px'>27</span>
+                 </td><td width='30%'>
+                 <img class='logo_faction' src='images/theme/defaut/logo_horde.PNG ' />
+
+                 <span style='font-size: 9px; position: relative; top:-4px; left:-3px'>13</span>
+                 </td><td width='40%'>
+                 <span class='test' style='position:relative;'>
+                 <img class='logo_faction' src='images/theme/defaut/logo_total.PNG' />
+                 <span style='font-size: 11px; position: relative; top:-4px; left:-3px'><strong>50</strong></span>
+                 </span>
+                 </td>
+                 </tr>
+                 </table>
+				</div>
+                </div>
+				<br />
 			</td>
 		</tr>
 		<tr>
 			<td class='statut_footer'>
-            <?php 
-			$ntj= $nta2+$nth2;
-			
-                 echo"<span class='login_royaume'>$ntj"; ?></span>
+			<span class='login_royaume'>350</span>
 		</td>
 	</tr>
 </table>
